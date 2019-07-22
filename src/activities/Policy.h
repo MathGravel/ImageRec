bccb03d6d06351f384c6b4e0c643bbd216ea0351
Jaël Champagne Gareau <champagne_gareau.jael@courrier.uqam.ca>
2019-07-22 06:24:27 -0400

    Policy.h partiel

--- src/activities/Policy.h
+++ src/activities/Policy.h
@@ -1,3 +1,12 @@
+/**
+* \file Policy.h
+* \author Mathieu Gravel
+* \version
+* \date
+* \brief
+* \details
+**/
+
 #ifndef POLICY_H
 #define POLICY_H
 
@@ -18,10 +27,35 @@ using namespace std;
 class Policy {
 
 public:
+    /**
+    * \fn	Policy()
+    * \brief	Constructor of Policy
+    **/
     Policy();
+
+    /**
+     * \fn update(Affordance* observation)
+     * \brief adds the current observation in gP
+     * \param observation : Affordance* observation
+     */
     bool update(Affordance* observation);
+
+    /**
+     * \fn Reset()
+     * \brief gP becomes a new solver
+     */
     void Reset();
+
+    /**
+     * @brief getNextActions
+     * @return
+     */
     std::vector<std::pair<std::string,float>> getNextActions();
+
+    /**
+     * @brief getGoalsProba
+     * @return
+     */
     std::vector<std::pair<std::string,float>> getGoalsProba();
 
 private:
